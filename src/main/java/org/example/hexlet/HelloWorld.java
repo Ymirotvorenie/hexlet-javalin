82d6bc752ab95345bf74e69a43e8ce2e9bef0269
ymirotvorenie <karoline.navarra@gmail.com>
2024-06-17 08:53:27 +0700

    queryParams

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,6 +10,14 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+            ctx.result(String.format("Hello, %s!", name));
+        });
+
         app.start(7070); // Стартуем веб-сервер
     }
 }
