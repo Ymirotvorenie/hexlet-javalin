5d16136db609c883c16a5a75184de8a358566242
ymirotvorenie <karoline.navarra@gmail.com>
2024-06-18 12:01:37 +0700

    gte

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,17 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("gg.jte:jte:3.1.9")
+
+    //JTE
+    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor ("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.test {
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,15 +1,25 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import org.example.hexlet.dto.courses.CoursePage;
+import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.model.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.render("index.jte"));
 
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
@@ -24,9 +34,23 @@ public class HelloWorld {
             ctx.result(String.format("User ID: %s, User PostID: %s", id, postId));
         });
 
+        app.get("/courses", ctx -> {
+            var courses = new ArrayList<Course>(List.of(
+                    new Course(1L,"Java", "Backend development"),
+                    new Course(2L,"JavaScript", "Fullstack development"),
+                    new Course(3L,"QA", "Big data development")));
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
+        });
+
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParam("id");
+            var course = new Course(10L, "name", "description");
+            var page = new CoursePage(course);
+            ctx.render("courses/show.jte", model("page", page));
+        });
 
         app.start(7070); // Стартуем веб-сервер
     }
 }
-
-//users/{id}/post/{postId}
